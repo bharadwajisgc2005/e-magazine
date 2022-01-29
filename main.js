df052a54a70b4246900497ddd8214ee717f7f4fe
Bharadwaj <bharadwajisgc2005@gmail.com>
2022-01-30 00:29:06 +0530

    commit 35

--- main.js
+++ main.js
@@ -12,57 +12,6 @@ video.onended = function(e) {
         skip.style.zIndex = -1;
     }, delayInMilliseconds);
 };
-var req;
-
-function reloadData()
-{
-   var now = new Date();
-   url = 'liveData?' + now.getTime();
-
-   try {
-      req = new XMLHttpRequest();
-   } catch (e) {
-      try {
-         req = new ActiveXObject("Msxml2.XMLHTTP");
-      } catch (e) {
-         try {
-            req = new ActiveXObject("Microsoft.XMLHTTP");
-         } catch (oc) {
-            alert("No AJAX Support");
-            return;
-         }
-      }
-   }
-
-   req.onreadystatechange = processReqChange;
-   req.open("GET", url, true);
-   req.send(null);
-}
-
-function processReqChange()
-{
-   // If req shows "complete"
-   if (req.readyState == 4)
-   {
-      dataDiv = document.getElementById('currentData');
-
-      // If "OK"
-      if (req.status == 200)
-      {
-         // Set current data text
-         dataDiv.innerHTML = req.responseText;
-
-         // Start new timer (1 min)
-         timeoutID = setTimeout('reloadData()', 60000);
-      }
-      else
-      {
-         // Flag error
-         dataDiv.innerHTML = '<p>There was a problem retrieving data: ' + req.statusText + '</p>';
-      }
-   }
-}
-
 function skipVid(){
     video.pause();
     setTimeout(function() {
